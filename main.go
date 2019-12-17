2180db83124ad8a7a2fcf05622a3406e6e96ebc0
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-12-17 10:15:28 +0900

    hello world

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 )
@@ -8,9 +9,9 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
+		fmt.Fprintf(w, "Hello World")
 	})
-	err := http.ListenAndServe(":" + port, nil)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		panic(err)
 	}
